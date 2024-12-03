f4df152b0ac762840e22072ff1e40e66186829f2
JojiSamuel123 <114049315+JojiSamuel123@users.noreply.github.com>
2024-12-03 10:31:47 +0530

    Initial commit

---
+++ SwiftUI Reference/SwiftUI_ReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ReferenceApp.swift
+//  SwiftUI Reference
+//
+//  Created by JOJI SAMUEL on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
